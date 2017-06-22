9d9cb1dfa784f99d381bda32b551ac37888b3cfb
MingZai <cmm_wy@163.com>
2017-06-22 09:33:37 +0800

    x

---
+++ SwiftAll/MZRootViewController.swift
@@ -0,0 +1,26 @@
+//
+//  MZRootViewController.swift
+//  SwiftAll
+//
+//  Created by Ming on 2017/6/20.
+//  Copyright © 2017年 Ming. All rights reserved.
+//
+
+import UIKit
+
+class MZRootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+
+    
+    
+
+}
